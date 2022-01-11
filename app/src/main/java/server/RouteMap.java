4c061bba22e445685ddcfdabdba8452b41bed43e
plopeziv <plopez@8thlight.com>
2022-01-11 17:20:27 -0600

    Complete Route List Insertions

--- app/src/main/java/server/RouteMap.java
+++ app/src/main/java/server/RouteMap.java
@@ -12,6 +12,7 @@ public class RouteMap {
     private HashMap<String, Route> constructRouteList() {
         HashMap<String, Route> startupList = new HashMap<>();
 
+
         ArrayList<String> simpleGetMethods = new ArrayList<>(Arrays.asList("GET", "HEAD", "OPTIONS"));
         RouteBehavior simpleGet  = (String HTTPVersion, String headers, String body) ->
                 HTTPVersion + " 200 OK\r\n" + headers + "\r\n\r\n";
@@ -21,24 +22,52 @@ public class RouteMap {
 
         ArrayList<String> simpleGetWithBodyMethods = new ArrayList<>(Arrays.asList("GET", "HEAD", "OPTIONS"));
         RouteBehavior simpleGetWithBody = (String HTTPVersion, String headers, String body) ->
-                HTTPVersion + " 200 OK\r\n" + headers + "\r\n" + "Hello World";
+                HTTPVersion + " 200 OK\r\n\r\n" + "Hello world";
 
         startupList.put("/simple_get_with_body", new Route("/simple_get_with_body",
                 simpleGetWithBodyMethods, simpleGetWithBody));
 
+
+        ArrayList<String> redirectMethods = new ArrayList<>(Arrays.asList("GET", "HEAD", "OPTIONS"));
+        RouteBehavior redirect = (String HTTPVersion, String headers, String body) ->
+                HTTPVersion + " 301 Moved Permanently\r\n" +  "Location: http://127.0.0.1:5000/simple_get\r\n\r\n";
+
+        startupList.put("/redirect", new Route("/redirect", redirectMethods,
+                redirect));
+
+
+        ArrayList<String> headRequestMethods = new ArrayList<>(Arrays.asList("HEAD", "OPTIONS"));
+        RouteBehavior headRequest = (String HTTPVersion, String headers, String body) ->
+                HTTPVersion + " 200 OK\r\n" + headers + "\r\n\r\n";
+
+        startupList.put("/head_request", new Route("/head_request", headRequestMethods,
+                headRequest));
+
+
         ArrayList<String> methodOptionsMethods = new ArrayList<>(Arrays.asList("GET", "HEAD", "OPTIONS"));
-        RouteBehavior methodOptions = (String HTTPVersion, String headers, String body) ->
-                HTTPVersion + " 200 OK\r\n" + "Allow: GET, HEAD, OPTIONS";
+        RouteBehavior methodOptions  = (String HTTPVersion, String headers, String body) ->
+                HTTPVersion + " 200 OK\r\n" + headers + "\r\n\r\n";
 
-        startupList.put("/method_options", new Route("/method_options", methodOptionsMethods,
-                methodOptions));
+        startupList.put("/method_options", new Route( "/method_options", methodOptionsMethods, methodOptions));
 
-        ArrayList<String> methodOptionsTwoMethods = new ArrayList<>(Arrays.asList("GET", "HEAD", "OPTIONS", "PUT", "POST"));
-        RouteBehavior methodOptionsTwo = (String HTTPVersion, String headers, String body) ->
-                HTTPVersion + " 200 OK\r\n" + "Allow: GET, HEAD, OPTIONS, PUT, POST";
 
-        startupList.put("/method_options2", new Route("/method_options2", methodOptionsTwoMethods,
+        ArrayList<String> methodOptionsTwoMethods = new ArrayList<>(Arrays.asList("GET", "HEAD", "OPTIONS",
+                "PUT", "POST"));
+        RouteBehavior methodOptionsTwo  = (String HTTPVersion, String headers, String body) ->
+                HTTPVersion + " 200 OK\r\n" + headers + "\r\n\r\n";
+
+        startupList.put("/method_options2", new Route( "/method_options2", methodOptionsTwoMethods,
                 methodOptionsTwo));
+
+
+        ArrayList<String> echoBodyMethods = new ArrayList<>(Arrays.asList("POST", "OPTIONS"));
+        RouteBehavior echoBody = (String HTTPVersion, String headers, String body) ->
+                HTTPVersion + " 200 OK\r\n" + headers + "\r\n" + body;
+
+        startupList.put("/echo_body", new Route("/echo_body", echoBodyMethods,
+                echoBody));
+
+
 
         return startupList;
     }
